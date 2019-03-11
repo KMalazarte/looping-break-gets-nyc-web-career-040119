bf497480028f0213a6ca3bfb37d6617b2a9f4dbe
Kevin Malazarte <kevin.malazarte@gmail.com>
2019-03-11 02:51:42 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,5 +1,12 @@
-
 def levitation_quiz
+  loop do
+    puts "What's up, Doc?"
+    answer = gets.chomp
+    break if answer == "Wingardium Leviosa"
+  end 
+  puts "You passed the quiz!"
+end
+  
 	#your code here
 end
 
